ca645e6a608d1f2d576fa2c787a63b2c1433f9d0
Sebastian <seba_al2007@hotmail.com>
2022-03-24 10:46:59 -0300

    proyectoMadre

---
+++ src/main/java/alkemy/alkemyy/AlkemyyApplication.java
@@ -0,0 +1,13 @@
+package alkemy.alkemyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkemyyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkemyyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alkemy/alkemyy/AlkemyyApplicationTests.java
@@ -0,0 +1,13 @@
+package alkemy.alkemyy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkemyyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
