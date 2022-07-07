e0b1998730d4921e23846194db478b24ff0ec4f2
Sebastian <seba_al2007@hotmail.com>
2022-07-07 17:48:57 -0300

    algomas

---
+++ src/main/java/alkemy/alkemyy/entities/Genero.java
@@ -0,0 +1,17 @@
+
+package alkemy.alkemyy.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Genero {
+    private String name;
+    private String image;
+    private Movie movie;
+   
+}
